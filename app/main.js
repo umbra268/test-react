f8eb227a4f89654de24e767e584c161dd97543a2
Joseph Zachariah Sim <simjza@gmail.com>
2017-03-26 21:49:34 +1300

    Add react router. Change posts.json to pages.json

--- app/main.js
+++ app/main.js
@@ -1,56 +1,69 @@
-(function(React, ReactDOM, Reactstrap) {
-  "use strict";
-  var h = React.createElement;
+// (function(React, ReactDOM, Reactstrap) {
+"use strict";
+var h = React.createElement;
 
-  var Navbar = React.createClass({
-    getInitialState: function getInitialState() {
-      return { open: false };
-    },
-    toggleOpen: function toggleOpen() {
-      this.setState({ open: !this.state.open });
-    },
-    render: function render() {
-      return React.createElement(
-        Reactstrap.Navbar,
-        { color: "faded", light: true, toggleable: true },
-        React.createElement(Reactstrap.NavbarToggler, { right: true, onClick: this.toggleOpen }),
+var Navbar = React.createClass({
+  getInitialState: function getInitialState() {
+    return { open: false };
+  },
+  toggleOpen: function toggleOpen() {
+    this.setState({ open: !this.state.open });
+  },
+  render: function render() {
+    return React.createElement(
+      Reactstrap.Navbar,
+      { color: "faded", light: true, toggleable: true },
+      React.createElement(Reactstrap.NavbarToggler, { right: true, onClick: this.toggleOpen }),
+      React.createElement(
+        Reactstrap.NavbarBrand,
+        { href: "/" },
+        "Test Site"
+      ),
+      React.createElement(
+        Reactstrap.Collapse,
+        { isOpen: this.state.open, navbar: true },
         React.createElement(
-          Reactstrap.NavbarBrand,
-          { href: "/" },
-          "Test Site"
-        ),
-        React.createElement(
-          Reactstrap.Collapse,
-          { isOpen: this.state.open, navbar: true },
+          Reactstrap.Nav,
+          { navbar: true },
           React.createElement(
-            Reactstrap.Nav,
-            { navbar: true },
+            Reactstrap.NavItem,
+            null,
             React.createElement(
-              Reactstrap.NavItem,
-              null,
-              React.createElement(
-                Reactstrap.NavLink,
-                { href: "/" },
-                "Test Link"
-              )
+              Reactstrap.NavLink,
+              { href: "/" },
+              "Test Link"
             )
           )
         )
       )
-    }
+    )
+  }
+});
+
+function layout() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(Navbar)
+  );
+}
+
+fetch('/pages.json')
+  .then(function(response) {
+    if(response.ok) { return response.json(); }
+    throw new Error('Network response was not ok.');
+  })
+  .then(function(pages) {
+    window.pages = pages;
+    ReactDOM.render(layout(), document.getElementById('root'));
+  })
+  .catch(function(error) {
+    console.log('There has been a problem with your fetch operation: ' + error.message);
   });
 
-  function layout() {
-    return React.createElement(
-      "div",
-      null,
-      React.createElement(Navbar)
-    );
-  }
+  // ReactDOM.render(layout(), document.getElementById('root'));
+// })(React, ReactDOM, Reactstrap);
 
-  ReactDOM.render(layout(), document.getElementById('root'));
-})(React, ReactDOM, Reactstrap);
-
-delete React;
-delete ReactDOM;
-delete Reactstrap;
+// delete React;
+// delete ReactDOM;
+// delete Reactstrap;
